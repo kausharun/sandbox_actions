f62ec76475230490595ce1128c404915182f06c6
kausharun <kaush.arunkumar@gmail.com>
2026-09-01 13:52:12 -0700

    add linter requested changes

--- test.py
+++ test.py
@@ -1,12 +1,15 @@
 from datetime import date
 
+
 def print_date():
     today = date.today()
     print(f"Today's date is {today}")
 
+
 def main():
     print("\n\nHey Kaushik, good afternoon!\n\n")
     print_date()
+
 
 if __name__ == "__main__":
     main()
