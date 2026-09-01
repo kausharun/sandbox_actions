2396681843017dfe863eae685306f3c02b1abb77
kausharun <kaush.arunkumar@gmail.com>
2026-09-01 13:26:09 -0700

    add date printer func

--- test.py
+++ test.py
@@ -1,5 +1,12 @@
+from datetime import date
+
+def print_date():
+    today = date.today()
+    printf(f"Today's date is {today}")
+
 def main():
     print("\n\nHey Kaushik, good afternoon!\n\n")
+    print_date()
 
 if __name__ == "__main__":
     main()
